ccd10cb4a0bf3ec08c46955ba74f1adb6565fcdf
genekomarov <gene.komarov@gmail.com>
2021-04-04 20:28:29 +0500

    First gulp task added and working

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,26 @@
-// Определяем константы Gulp
-const { src, dest, parallel, series, watch } = require('gulp');
+// Константы gulp
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+
+// Константы yargs
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
+// Константы проекты
+const tsProject = ts.createProject('tsconfig.json');
+
+function build(cb) {
+    if (argv.file && typeof argv.file === 'string') {
+        const file = argv.file;
+        return gulp.src(file)
+            .pipe(tsProject()).js
+            .pipe(gulp.dest('dist'));
+    }
+    return tsProject.src()
+        .pipe(tsProject()).js
+        .pipe(gulp.dest('dist'));
+    cb();
+}
+
+module.exports.build = build;
