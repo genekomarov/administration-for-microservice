72d845f14355735a59b9891247ce5461261ef239
genekomarov <gene.komarov@gmail.com>
2021-04-10 13:26:14 +0500

    Reformat

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 
 // Константы yargs
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+const yargs = require('yargs/yargs');
+const {hideBin} = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
 
 // Константы проекты
 const tsProject = ts.createProject('tsconfig.json');
 
-function build(cb) {
+function build() {
     if (argv.file && typeof argv.file === 'string') {
         const file = argv.file;
         return gulp.src(file)
@@ -20,7 +20,6 @@ function build(cb) {
     return tsProject.src()
         .pipe(tsProject()).js
         .pipe(gulp.dest('dist'));
-    cb();
 }
 
 module.exports.build = build;
